645243366deb69a0f7e50c356517368d610c53b6
alepp013 <34256400+alepp013@users.noreply.github.com>
2017-12-04 21:55:59 -0500

    Add files via upload

---
+++ AutoDealer.java
@@ -0,0 +1,38 @@
+
+
+public class  AutoDealer{
+  
+  private Hand hand;
+  
+  private Deck deck;
+  
+  public AutoDealer(Deck d){
+    this.deck = d;
+    this.hand = new Hand();
+  }
+  
+  public int startRound(){
+    hand.draw(deck.draw());
+    return hand.getCards()[1];
+  }
+  
+  public void play(){
+    while(hand.getScore() < 17){
+      hand.draw(deck.draw());
+    }
+  }
+  
+  public boolean didPlayerWin(int pScore){
+    if (pScore > 21 ||(pScore < hand.getScore() && hand.getScore() < 22))
+      return false;
+    else
+      return true;
+  }
+  
+  public boolean didPlayerTie(int pScore){
+    if ((pScore > 21 && hand.getScore() > 21) ||(pScore == hand.getScore()))
+      return true;
+    else
+      return false;
+  }
+}
