fcb4cfc71924562ce3b88bb9e2924cbc922cd5e2
alepp013 <34256400+alepp013@users.noreply.github.com>
2017-12-05 21:33:16 -0500

    Add files via upload

---
+++ BJClient.java
@@ -0,0 +1,47 @@
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.Socket;
+import java.util.Scanner;
+import java.net.InetAddress;
+
+public class BJClient{
+  
+  private Socket socket;
+  private BufferedReader in;
+  private PrintWriter out;
+  private String userName;
+  
+  private Scanner userInput;
+  
+  private boolean playing;
+  
+  public BJClient(String userName) throws IOException{
+    this.playing = true;
+    this.userName = userName;
+    this.userInput = new Scanner(System.in);
+    this.socket = new Socket(InetAddress.getByName("127.0.0.1" ), 9898);
+    
+    in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+    out = new PrintWriter(socket.getOutputStream(), true);
+    
+    while(playing){
+      
+      // get server message
+      
+      // print server message
+      
+      // get response from user
+      
+      // if "stop" set playing to "false"
+      // else send response to server
+      
+    }
+    
+    this.socket.close();
+    
+  }
+  
+}
