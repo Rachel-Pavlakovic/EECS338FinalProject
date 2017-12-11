4a7db24566a0cb615d21326cb8dd90a836c0105f
Rachel Pavlakovic <rachelpav@comcast.net>
2017-12-11 14:40:55 -0500

    Add files via upload

--- Server.java
+++ Server.java
@@ -1,155 +1,77 @@
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
 import java.io.ObjectInputStream;
 import java.io.ObjectOutputStream;
 import java.net.ServerSocket;
 import java.net.Socket;
 import java.util.concurrent.ConcurrentHashMap;
 
+import org.omg.CORBA.portable.OutputStream;
+
 /**
  * 
  * @author Andrew Leppo and Rachel Pavlakovic
  *
  */
-public class Server {
+public class Server extends Thread{
+	public static final int portNum = 9898;
 	
-	private static Deck deck;
-	private static Hand hand;
-	private static AutoDealer dealer;
+	protected Socket socket;
 	
-	/**
-	 * Main Method
-	 * @param args
-	 * @throws IOException
-	 */
-	public static void main(String[] args) throws IOException {
-		ConcurrentHashMap<String, Integer> chips = new ConcurrentHashMap<String, Integer>();
-		ServerSocket listener = new ServerSocket(9898);
-		 
-		try{
-			 while (true) {
-				 new Seat(listener.accept(), chips);
-			 }
-		 }
-		 finally {
-			 listener.close();
-		 }
+	private Server(Socket socket) {
+		this.socket = socket;
+		System.out.println("New client connected from " + socket.getInetAddress().getHostAddress());
+		start();
 	}
 	
-	/**
-	 * 
-	 * Static inner class that represents a seat at the table
-	 *
-	 */
-	private static class Seat extends Thread {
-		private Socket socket;
-		private ConcurrentHashMap<String, Integer> chipCount;
-		private ObjectOutputStream output; 
-		private ObjectInputStream input; 
-		
-		public Seat(Socket socket, ConcurrentHashMap<String, Integer> chipCount) {
-			this.socket = socket;
-			this.chipCount = chipCount;
-		}
-		
-		public void run() {
-			try {
-				try {
-					getStreams();
-					processConnection(); 
-				} catch (IOException e) {
-					e.printStackTrace();
-				} 
-			}
-			finally {
-				try {
-					socket.close();
-				}
-				catch (IOException e) {
-					log("Couldn't close socket");
-				}
-				log("Connect with client closed");
+	public void run() {
+		InputStream in = null;
+		OutputStream out = null;
+		try {
+			in = socket.getInputStream();
+			out = (OutputStream) socket.getOutputStream();
+			BufferedReader reader = new BufferedReader(new InputStreamReader(in));
+			String request;
+			while((request = reader.readLine()) != null) {
+				System.out.println("Message received:" + request);
+				request += '\n';
+				out.write(request.getBytes());
 			}
 		}
-		
-		/**
-		 * 
-		 * @throws IOException
-		 */
-		private void getStreams() throws IOException {
-			
-			output = new ObjectOutputStream( socket.getOutputStream() );
-			output.flush(); 
-
-			input = new ObjectInputStream( socket.getInputStream() );
-
-		} 
-		
-		/**
-		 * 
-		 * @throws IOException
-		 */
-		private void processConnection() throws IOException {
-			String message = "Connection successful";
-			sendData(message); 
-			
-			dealer = new AutoDealer(deck);
-			
-			do {
-			 
-				try {
-					if(message.contains("hit")) {				
-						cardhit();
-					}
-
-					if(message.contains("stay")) {
-						dealer.clearHand();
-						dealer.startRound();
-						dealer.play();
-						
-						boolean playerWins = dealer.didPlayerWin(hand.getScore());
-						boolean playerTies = dealer.didPlayerTie(hand.getScore());
-					}
-
-					message = ( String ) input.readObject(); 
-
-				} 
-				catch ( ClassNotFoundException classNotFoundException ) {
-				} 
-
-			} 
-			
-			while ( !message.equals( "CLIENT>>> TERMINATE" ) );
-		} 
-		
-		/**
-		 * 
-		 */
-		private void cardhit() {
-			hand.addCard(deck.draw());
-			
-			sendData("Your Total: " +  hand.getScore());
+		catch(IOException e) {
+			System.out.println("Unable to get streams from client");
 		}
-		
-		/**
-		 * 
-		 * @param message
-		 */
-		private void sendData( String message ) {
-			try {
-				output.writeObject( message );
-				output.flush(); 
-			} 
-			catch ( IOException ioException ) {
-			} 
-		} 
-	
-		/**
-		 * 
-		 * @param message
-		 */
-		private void log(String message) {
-			System.out.println(message);
+		finally {
+			try{
+				in.close();
+				out.close();
+				socket.close();
+			}
+			catch(IOException e) {
+				e.printStackTrace();
+			}
 		}
 	}
 	
+	public static void main(String[] args) {
+		System.out.println("SocketServer Example");
+		ServerSocket server = null;
+		try {
+			server = new ServerSocket(portNum);
+			while (true) {
+				new Server(server.accept());
+			}
+		} catch (IOException ex) {
+			System.out.println("Unable to start server.");
+		} finally {
+			try {
+				if (server != null)
+					server.close();
+			} catch (IOException ex) {
+				ex.printStackTrace();
+			}
+		}
+	}
 }
