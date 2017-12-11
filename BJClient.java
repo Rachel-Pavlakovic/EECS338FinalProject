66414dccdde8a09aea57725347e49667f90dafac
Rachel Pavlakovic <rachelpav@comcast.net>
2017-12-11 14:52:11 -0500

    Add files via upload

--- BJClient.java
+++ BJClient.java
@@ -3,6 +3,7 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.PrintWriter;
 import java.net.Socket;
+import java.net.UnknownHostException;
 import java.util.Scanner;
 import java.net.InetAddress;
 
@@ -13,56 +14,64 @@ import java.net.InetAddress;
  */
 public class BJClient{
   
-  private Socket socket;
-  private BufferedReader in;
-  private PrintWriter out;
-  private String userName;
-  private Scanner userInput;
-  private boolean playing;
-  private String message;
-  
-  /**
-   * Constructor for the BJClient
-   * @param userName
-   * @throws IOException
-   */
-  public BJClient(String userName) throws IOException{
-    this.playing = true;
-    this.userName = userName;
-    this.userInput = new Scanner(System.in);
-    this.socket = new Socket(InetAddress.getByName("127.0.0.1" ), 9898);
-    
-    in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
-    out = new PrintWriter(socket.getOutputStream(), true);
-	BufferedReader stdIn = new BufferedReader(new InputStreamReader(System.in));
-	
-    while(playing){
-      
-      // get server message
-    	message = in.readLine();
-      
-      // print server message
-    	System.out.println(message);
-      
-      // get response from user
-    	String userInput = stdIn.readLine();
-      
-      // if "stop" set playing to "false"
-    	if(userInput.equals("stop")) {
-    		playing = false;
-    	}
-      // else send response to server
-    	else {
-    		out.println(userInput);
-    	}
-      
+	public static void main(String args[]) {
+        String host = "127.0.0.1";
+        int port = 9898;
+        new BJClient(host, port);
     }
-    
-    out.close();
-    in.close();
-    stdIn.close();
-    this.socket.close();
-    
-  }
-  
+ 
+	public BJClient(String host, int port) {
+		try {
+			String serverHost = new String("127.0.0.1");
+			System.out.println("Connecting to host " + serverHost + " on port " + port + ".");
+			 
+            Socket socket = null;
+            PrintWriter out = null;
+            BufferedReader in = null;
+ 
+            try {
+                socket = new Socket(serverHost, 8081);
+                out = new PrintWriter(socket.getOutputStream(), true);
+                in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+            } catch (UnknownHostException e) {
+                System.err.println("Unknown host: " + serverHost);
+                System.exit(1);
+            } catch (IOException e) {
+                System.err.println("Unable to get streams from server");
+                System.exit(1);
+            }
+ 
+            BufferedReader stdIn = new BufferedReader(new InputStreamReader(System.in));
+            
+            while(true){
+            	String message = null;
+            	
+            	while(message == null) {
+            		// get server message
+            		message = in.readLine();
+            	}
+                
+                // print server message
+              	System.out.println(message);
+              	message = null;
+                
+                // get response from user
+              	String userInput = stdIn.readLine();
+                
+              	if(userInput.equals("stop")) {
+              		break;
+              	}
+                // else send response to server
+              	else {
+              		out.println(userInput);
+              	}
+            }
+            out.close();
+            in.close();
+            stdIn.close();
+            socket.close();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+	}
 }
